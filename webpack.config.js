dd335a57be78aaffe41ca5849971ff2553c95fdc
Jeremy Magland <jeremy.magland@gmail.com>
2018-03-01 04:10:13 -0500

    initial

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'web')
+  },
+  module:{
+    rules:[
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src']
+          }
+        }
+      }
+    ]
+  }
+};
